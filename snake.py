7f19e1ee5321cdc13c142893afd532d50d81d9d8
Stoyan Stoyanov <djstoxer@gmail.com>
2012-06-26 20:03:29 +0300

    small changes. now using constants from module

--- snake.py
+++ snake.py
@@ -1,28 +1,7 @@
 import pygame
 import random
 from pygame.locals import *
-
-DISPLAY_SIZE = (640, 480)
-
-RIGHT = 1
-LEFT = 2
-DOWN = 3
-UP = 4
-
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-CYAN = (0, 255, 255)
-
-REGULAR_START = 0
-RANDOM_START = 1
-QUIT_GAME = 2
-
-SNAKE_REC = (7, 9)
-FOOD_REC = (10, 10)
-OBSTAN_REC = (10, 10)
-START_SPEED = 100
-TIME_DISAPPEAR = 8.0
+from snakegamesysdata import *
 
 
 class Snake(pygame.sprite.Sprite):
